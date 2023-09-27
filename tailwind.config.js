492f83c1d042b9864c2902b825e95ed0f2904cbe
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-27 20:25:22 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    fontFamily: {
+      'overpass': ['Overpass', 'sans']
+    },
+    extend: {
+      colors: {
+        'lightgrey': '#959eac',
+        'mediumgrey': '	#7c8798',
+        'darkblue': '#252d37',
+        'verydarkblue': '	#121417'
+      },
+    }
+  },
+  plugins: [],
+}
+
